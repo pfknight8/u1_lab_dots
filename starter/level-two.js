f15a7319a910c12eccfc114c96c0b2bdef15e541
Ben Manning <bmanning@me.com>
2022-06-24 14:43:38 -0400

    added the solution for level three

--- starter/level-two.js
+++ starter/level-two.js
@@ -4,7 +4,7 @@ let scoreDisplay = document.querySelector('.js-score');
 let levelWinner = document.querySelector('.level-winner');
 let balls = document.querySelectorAll('.js-ball');
 
-
+// event
 for (let i = 0; i < balls.length; i++) {
     balls[i].addEventListener('click', () => {
         score += 10;
